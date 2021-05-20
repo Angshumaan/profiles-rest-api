01c44ef354ca96a18cec88323316d81b72d1cd69
= <=>
2021-05-20 20:22:55 +0530

    Django admin for user

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -10,19 +10,20 @@ class UserProfileManager(BaseUserManager):
         """create a new user profile"""
         if not email:
             raise ValueError('User must have an email address')
-        email = self.normalize_email(emai)
+        email = self.normalize_email(email)
         user = self.model(email=email,name=name)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-        def create_superuser(self,email,name,password):
-            """Create and save a new superuser with given details"""
-            user = self.create_user(email,name,password)
-            user.is_superuser = True
-            user.is_staff = True
+    def create_superuser(self,email,name,password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email,name,password)
 
-            return user
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
 
 
 
@@ -38,6 +39,8 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     USERNAME_FIELD = 'email'
 
     REQUIRED_FIELDS = ['name']
+
+
 
     def get_full_name(self):
         """Retrieve full name of user"""
