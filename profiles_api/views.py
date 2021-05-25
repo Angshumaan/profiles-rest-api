dd9cabcbf1ee474c024452465b8793082158c89c
= <=>
2021-05-25 21:01:25 +0530

    login

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from  rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from . serializers import HelloSerializer,UserProfileSerializer
 from . models import UserProfile
 from . permissions import UpdateOwnProfile
@@ -112,3 +114,7 @@ class UserProfileViewset(viewsets.ModelViewSet):
     permission_classes = (UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name','email')
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
